b6d2e0ea7ac94bf7dbe83044f0c18275ccb5813f
66160148 <66160148@go.buu.ac.th>
2024-12-21 18:07:08 +0700

    readme

--- js/script.js
+++ js/script.js
@@ -4,31 +4,4 @@ document.addEventListener('DOMContentLoaded', () => {
     toggler.addEventListener("click", function () {
         togglerIcon.classList.toggle("open");
     });
-    filters.forEach(filter => {
-        filter.addEventListener('click', () => {
-            // Remove 'active' class from all buttons
-            filters.forEach(f => f.classList.remove('active'));
-
-            // Add 'active' class to the clicked button
-            filter.classList.add('active');
-
-            const category = filter.dataset.filter;
-
-            items.forEach(item => {
-                // Hide all items initially
-                item.style.opacity = '0';
-
-                setTimeout(() => {
-                    if (category === 'all' || item.classList.contains(category)) {
-                        item.style.display = 'block';
-                        setTimeout(() => {
-                            item.style.opacity = '1';
-                        }, 50);
-                    } else {
-                        item.style.display = 'none';
-                    }
-                }, 300);
-            });
-        });
-    });
 });
